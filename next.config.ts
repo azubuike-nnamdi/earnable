b7e8e6be163658545f5443a5cf399bbc62584c2a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-03-01 20:27:21 +0100

    chore: add redirect :fire:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async function () {
+    const redirects = [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ]
+    return redirects
+  },
 };
 
 export default nextConfig;
